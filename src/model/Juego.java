6acf9d50b3a02f69332b03cd93a9fe1771b81517
diana-sanchez-de-la-cruz <16470289@itcampeche.edu.mx>
2017-06-14 18:26:10 -0500

    Primera versión completa del proyecto.

---
+++ src/model/Juego.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Sanchez de la Cruz
+ */
+public class Juego {
+    private NumeroSecreto numeroSecreto; 
+    private int limiteInferior;
+    private int limiteSuperior;
+    private int numeroDeIntentos;
+    private int intentosRealizados;
+    private static final int LIM_INF_PREDET = 1;
+    private static final int LIM_SUP_PREDET = 100;
+
+    public Juego(int i, int i0, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    public void setRangoDelNumeroSecreto(int limiteInfeerior, int limiteSuperior){
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+        numeroSecreto.setRango(this.limiteInferior, this.limiteSuperior);
+        this.intentosRealizados = 0;
+        
+    }
+    
+    public void setNumeroDeIntentos (int intentos){
+        this.numeroDeIntentos = intentos;
+    }
+    
+    public boolean esElNumeroSecreto (int numero){
+        this.intentosRealizados++;
+        return numeroSecreto.esIgual(numero);
+    }
+    public boolean puedeIntentarDeNuevo() {
+        boolean hayMasOportunidades = this.intentosRestantes() > 0;
+        return hayMasOportunidades;
+    }
+    
+    public int intentosRestantes() {
+        int numeroDeIntentosRestantes = this.numeroDeIntentos - this.intentosRealizados;
+        return (numeroDeIntentosRestantes);
+    }
+    
+    public String getTextoDeAyuda(int numero) {
+        String texto = null;
+        if (this.numeroSecreto.esMayor(numero) == true) {
+            texto = "mayor";
+        }
+        else if (this.numeroSecreto.esMenor(numero) == true) {
+            texto = "menor";
+        }
+        return texto;
+}
+
+    public String getNumeroSecreto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    }
+    
+
+    
--- src/model/NumeroSecreto.java
+++ src/model/NumeroSecreto.java
@@ -5,10 +5,77 @@
  */
 package model;
 
+import java.util.Random;
+
 /**
  *
  * @author Sanchez de la Cruz
  */
 public class NumeroSecreto {
+    private int numero; 
+    private int limiteinferior;
+    private int limitesuperior;
+    private Random generador; 
+    private static final int LIM_INF_PREDET = 1;
+    private static final int LIM_SUP_PREDET = 100;
     
+    public void setRango (int limiteinferior, int limitesuperior){
+        if (limiteinferior > limitesuperior){
+            this.limiteinferior = LIM_INF_PREDET;
+            this.limitesuperior = LIM_SUP_PREDET;
+        }
+        else {
+            this.limiteinferior= limiteinferior;
+            this.limitesuperior= limitesuperior;
+        }
+        
+    }
+    public final void generar (){
+        this.numero=generador.nextInt(limitesuperior) + 1;
+        
+    }
+    public NumeroSecreto (int limiteinferior, int limitesuperior){
+        this.setRango(limiteinferior, limitesuperior);
+        this.generador= new Random();
+        this.generar();
+    }
+    public NumeroSecreto (){
+        this (LIM_INF_PREDET, LIM_SUP_PREDET);
+        
+    }
+    public int getNumeroSecreto (){
+        return this.numero;
+}
+    public int getlimiteinferio (){
+        return this.limiteinferior; 
+    }
+    public int getlimitesuperior (){
+        return this.limitesuperior;
+    }
+    public boolean esMayor (int numero){
+        boolean elNumeroSecretoesMayor;
+        if (this.numero > numero){
+            elNumeroSecretoesMayor = true;}
+        else{
+            elNumeroSecretoesMayor = false;
+          
+        }
+        return elNumeroSecretoesMayor;
+    }
+    public boolean esMenor (int numero){
+        boolean elNumeroSecretoesMenor = false;
+        if (this.numero < numero){
+            elNumeroSecretoesMenor =true;
+        }
+        return elNumeroSecretoesMenor;
+        
+    }
+    public boolean esigual (int numero){
+        return (this.numero == numero);
+    }
+
+    boolean esIgual(int numero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
 }
